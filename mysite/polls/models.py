c1a3608b5a25c54aee386b5cada7ae70ed45a8ed
jun-kyeong <ojk27974992@gmail.com>
2023-12-03 20:33:38 +0900

    [공식] 첫 번째 django 앱 작성하기 part7 끝

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 
@@ -13,6 +14,18 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now-datetime.timedelta(days=1)<=self.pub_date<=now
+    
+    @admin.display(
+        boolean = True,
+        ordering = "pub_date",
+        description= "Published recently?",
+    )
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days = 1) <= self.pub_date <= now
+    
+    
 class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
